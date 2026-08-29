9fc06915bd81471a91d101039c8d4439cb227f99
naeem-ebay <naeemkasuri01@gmail.com>
2026-08-29 19:14:33 +0100

    Update server run configuration with host and port

--- server.py
+++ server.py
@@ -88,4 +88,8 @@ async def ebay_search(
 
 
 if __name__ == "__main__":
-    mcp.run(transport="streamable-http")
+   mcp.run(
+    transport="streamable-http",
+    host="0.0.0.0",
+    port=int(os.environ.get("PORT", 10000))
+)
